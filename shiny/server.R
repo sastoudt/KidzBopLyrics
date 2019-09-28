12241c39102673d9380d7be48dd92964cd2aa4bb
sastoudt <sstoudt@berkeley.edu>
2019-09-27 18:10:51 -0700

    it works, now make it less gross

---
+++ shiny/server.R
@@ -0,0 +1,29 @@
+library(diffobj)
+server <- function(input, output, session) {
+  
+  load(file = "../data/kbLyrics.RData")
+  load(file = "../data/ogLyrics.RData")
+  load(file = "../data/OG_artist_KB.RData")
+  load(file = "../data/OG_artist_name_KB.RData")
+  kb <- read.csv("../data/kbSongs.csv", stringsAsFactors = F)
+  
+  
+  idx <- reactive({
+
+    which(kb$song_name == input$songChoice)
+    
+  })
+
+  output$diffobj_element <- renderUI({
+    HTML(
+      as.character(
+        diffPrint(
+          as.data.frame(ogLyrics[[idx()]]$line), as.data.frame(kbLyrics[[idx()]]$result$line),
+          mode = "sidebyside",
+          format = "html"#,
+          #style = list(html.output = "diff.w.style")
+        )
+      )
+    )
+  })
+}
---
+++ shiny/ui.R
@@ -0,0 +1,21 @@
+kb <- read.csv("../data/kbSongs.csv", stringsAsFactors = F)
+
+ui <- fluidPage(
+  titlePanel("Compare Kidz Bop Lyrics to the Original"),
+  fluidRow(
+    column(
+      3,
+      wellPanel(
+        hr("Pick a song"),
+        selectInput("songChoice", "Original Song", kb$song_name,
+          selected = NULL, multiple = FALSE,
+          selectize = TRUE, width = NULL, size = NULL
+        )
+      )
+    ),
+    column(
+      9,
+      htmlOutput("diffobj_element")
+    )
+  )
+)
